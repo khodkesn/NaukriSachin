d2bff35636bc116e1af092193cb25447d1925859
Sachin Khodke <khodkesn@gmail.com>
2018-10-02 19:33:13 +0530

    changed URL variable name

--- src/amazon_shopping/Amazon_Driver_Page.java
+++ src/amazon_shopping/Amazon_Driver_Page.java
@@ -7,13 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon_Driver_Page {
 	
-		static String URL = "https://www.amazon.in/";
+		static String URL1 = "https://www.amazon.in/";
 				
 		public static void main(String[] args) throws IOException, InterruptedException{
 			
 		System.setProperty("webdriver.chrome.driver", "C:/Users/khodk/Desktop/Drivrers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver(); 
-		driver.get(URL);
+		driver.get(URL1);
 		ReadExcel.readExcel("C:\\Users\\khodk\\Desktop\\Selenium Data", "Amazon Data.xlsx", "Credentials" );
 		driver.manage().window().maximize();
 	    Login.login(driver, "Valid");
